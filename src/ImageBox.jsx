19a28ff24ddd8b3c28b80a4420fb42b0bdd3a219
abhijithabhi01 <iamabhijithabhi01@gmail.com>
2024-04-25 07:23:26 +0530

    done for now

--- src/ImageBox.jsx
+++ src/ImageBox.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import './imagebox.css'
 import Carousel from 'react-bootstrap/Carousel';
 import cimg from'./images/cartoon.png'
+import pic1 from'./images/pic1.jpg'
+import pic2 from'./images/pic2.jpg'
+import pic3 from'./images/pic3.jpg'
+
 function ImageBox() {
   return (
     <>
 <div className='imageboxmain'>
 
 <Carousel>
+
+
+      <Carousel.Item>
+       <img src={pic1} alt="" className='cimage'/>
+      </Carousel.Item>
+      <Carousel.Item>
+       <img src={pic2} alt="" className='cimage'/>
+      </Carousel.Item>
+      <Carousel.Item>
+       <img src={pic3} alt="" className='cimage'/>
+      </Carousel.Item>
       <Carousel.Item>
        <img src={cimg} alt="" className='cimage'/>
       </Carousel.Item>
-
-      <Carousel.Item>
-       <img src={cimg} alt="" className='cimage'/>
-      </Carousel.Item>
-      <Carousel.Item>
-       <img src={cimg} alt="" className='cimage'/>
-      </Carousel.Item>
-
-
     </Carousel>
 
 </div>
